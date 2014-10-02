75999a3bb25b20158957079ca566b100e0e768fc
James Carr <james.r.carr@gmail.com>
2014-10-02 16:50:17 -0500

    working install for 14.04

--- ubuntu-14.04-server-amd64/scripts/cleanup.sh
+++ ubuntu-14.04-server-amd64/scripts/cleanup.sh
@@ -32,17 +32,11 @@ apt-get -y remove
 echo "Running apt-get auto-remove..."
 apt-get -y autoremove
 
-#Remove non-English locale files
-echo "Removing non-English locale files"
-sudo rm -rf /usr/share/locale/{af,am,ar,as,ast,az,bal,be,bg,bn,bn_IN,br,bs,byn,ca,cr,cs,csb,cy,da,de,de_AT,dz,el,en_AU,en_CA,eo,es,et,et_EE,eu,fa,fi,fo,fr,fur,ga,gez,gl,gu,haw,he,hi,hr,hu,hy,id,is,it,ja,ka,kk,km,kn,ko,kok,ku,ky,lg,lt,lv,mg,mi,mk,ml,mn,mr,ms,mt,nb,ne,nl,nn,no,nso,oc,or,pa,pl,ps,pt,pt_BR,qu,ro,ru,rw,si,sk,sl,so,sq,sr,sr*latin,sv,sw,ta,te,th,ti,tig,tk,tl,tr,tt,ur,urd,ve,vi,wa,wal,wo,xh,zh,zh_HK,zh_CN,zh_TW,zu}
-
 echo "pre-up sleep 2" >> /etc/network/interfaces
 
 #zero out disk space. Replacing free space with 0s makes the drive more easily compressed
 echo "Zeroing out disk..."
-cat /dev/zero > zero.fill;sync;sleep 1;sync;rm -f zero.fill
 
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
-exit
 
